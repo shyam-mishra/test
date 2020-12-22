4f6e330cc707cdc545c3ccb8a794ad861d15db0d
SutapCedcoss <sutapsahu@cedcoss.com>
2020-12-22 13:46:39 +0530

    create login feature

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
+class Login
+{
+    public function createLogin()
+    {
+        $userName = 'Sutap';
+        $password = 'password@123';
+    }
+}
 echo "Hello World";
 
 // added a comment just to check what is happening again
